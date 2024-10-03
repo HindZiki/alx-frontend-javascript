f4bfda64f69ab18609099c537cf6189a3ce8e26a
HindZiki <hind23051@gmail.com>
2024-10-03 09:37:40 +0000

    1

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,6 @@
+// 0-classroom.js
 export default class ClassRoom {
-  constructor(maxStudentsSize) {
-    this._maxStudentsSize = maxStudentsSize; // Assigner la valeur à _maxStudentsSize
-  }
+    constructor(maxStudentsSize) {
+        this._maxStudentsSize = maxStudentsSize;  // Assignation de la valeur à _maxStudentsSize
+    }
 }
