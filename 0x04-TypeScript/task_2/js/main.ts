f6d92a290b4e1947c8b942723be3e33240842bc4
HindZiki <hind23051@gmail.com>
2024-10-09 15:54:10 +0000

    4

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,13 @@
+// main.ts
+
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
