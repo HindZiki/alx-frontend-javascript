f413ca1348678068beed980a7bb4fa8476889254
HindZiki <hind23051@gmail.com>
2024-10-09 15:57:54 +0000

    4

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -11,3 +11,60 @@ interface TeacherInterface {
   getCoffeeBreak(): string;
   workTeacherTasks(): string;
 }
+
+// Interfaces for Director and Teacher
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// Director class implementing DirectorInterface
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
+
+// Teacher class implementing TeacherInterface
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+// Function to create an employee based on salary
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+// Testing the function createEmployee
+console.log(createEmployee(200));  // Should return a Teacher instance
+console.log(createEmployee(1000));  // Should return a Director instance
+console.log(createEmployee('$500'));  // Should return a Director instance
